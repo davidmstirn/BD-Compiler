384c33bc9feb1109228a4c6b0573f0cb0d266115
David Stirn <davidmstirn@gmail.com>
2023-02-01 13:16:26 -0500

    Initialize Project

---
+++ Compiler/src/main/java/com/bd/compiler/Compiler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bd.compiler;
+
+/**
+ *
+ * @author dajms
+ */
+public class Compiler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
